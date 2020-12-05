7ff7b9a5638e09562365a5fbf05344b252308f12
Jordan Powell <bballjordan@gmail.com>
2020-12-04 22:11:24 -0700

    Main.js added characters

--- main.js
+++ main.js
@@ -5,9 +5,17 @@ const main = document.querySelector('main')
 
 
 
-people.forEach(person => {
+/*people.forEach(person => {
     console.log(person.name)
     let personImg = document.createElement('img')
     personImg.src = "https://starwars-visualguide.com/assets/img/characters/1.jpg"
     main.appendChild(personImg)
 })
+*/
+
+for (let i = 0; i < 7; i++){
+    let personImg = document.createElement('img')
+    personImg.src = 'https://starwars-visualguide.com/assets/img/characters/' + (i + 1) + '.jpg'
+    main.appendChild(personImg)
+    console.log(people[i].name)
+}
