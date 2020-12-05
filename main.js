c711bf8c18522b9957a971b32d736eb13a938254
Jordan Powell <bballjordan@gmail.com>
2020-12-04 22:31:09 -0700

    Added Figs

--- main.js
+++ main.js
@@ -14,8 +14,15 @@ const main = document.querySelector('main')
 */
 
 for (let i = 0; i < 7; i++){
-    let personImg = document.createElement('img')
-    personImg.src = 'https://starwars-visualguide.com/assets/img/characters/' + (i + 1) + '.jpg'
-    main.appendChild(personImg)
-    console.log(people[i].name)
+    let figure = document.createElement('figure')
+    let figImg = document.createElement('img')
+    figImg.src = 'https://starwars-visualguide.com/assets/img/characters/' + (i + 1) + '.jpg'
+    let figCaption = document.createElement('figcaption')
+    figCaption.textContent = people[i].name
+
+    figure.appendChild(figImg)
+    figure.appendChild(figCaption)
+
+    main.appendChild(figure)
+    
 }
