beb0e5caaee5f4ecf2c2eb89b229397766f3fef4
Jordan Powell <bballjordan@gmail.com>
2020-12-11 21:23:59 -0700

    Lost 00's

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ function populatePokeCard(singlePokemon){
     let frontLabel = document.createElement('p')
     frontLabel.textContent = singlePokemon.name
     let frontImage = document.createElement('img')
-    frontImage.src = `../images/pokemon/00${getImageFileName(singlePokemon)}.png`
+    frontImage.src = `../images/pokemon/${getImageFileName(singlePokemon)}.png`
 
     pokeFront.appendChild(frontImage)
     pokeFront.appendChild(frontLabel)
