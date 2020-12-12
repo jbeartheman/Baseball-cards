dbb1fc6402b5dcb0d3200e6d1fbe35ec499e4ffd
Jordan Powell <bballjordan@gmail.com>
2020-12-11 22:16:28 -0700

    ADding flip and card color and info to back and front

--- main.js
+++ main.js
@@ -30,26 +30,48 @@ function populatePokeCard(singlePokemon){
     pokeScene.className = 'scene'
     let pokeCard = document.createElement('div')
     pokeCard.className = 'card'
-    let pokeFront = document.createElement('div')
+    var card = document.querySelector('.card');
+    pokeCard.addEventListener( 'click', function() {
+        pokeCard.classList.toggle('is-flipped')
+        })
+    
     let pokeBack = document.createElement('div')
+    pokeBack.className = 'card_face'
+    
 
-    let frontLabel = document.createElement('p')
-    frontLabel.textContent = singlePokemon.name
-    let frontImage = document.createElement('img')
-    frontImage.src = `../images/pokemon/${getImageFileName(singlePokemon)}.png`
-
-    pokeFront.appendChild(frontImage)
-    pokeFront.appendChild(frontLabel)
-    pokeCard.appendChild(pokeFront)
-    pokeCard.appendChild(pokeBack)
+    let backLabel = document.createElement('p')
+    backLabel.textContent = `${singlePokemon.moves.length} moves`
+    
+    pokeCard.appendChild(populateCardFront(singlePokemon))
+    pokeCard.appendChild(populateCardBack(singlePokemon))
     pokeScene.appendChild(pokeCard)
     pokeGrid.appendChild(pokeScene)
 }
 
+function populateCardFront(pokemon) {
+    let pokeFront = document.createElement('div')
+    pokeFront.className = 'card_face card_face--back'
+    let frontLabel = document.createElement('p')
+    frontLabel.textContent = pokemon.name
+    let frontImage = document.createElement('img')
+    frontImage.src = `../images/pokemon/${getImageFileName(pokemon)}.png`
+    pokeFront.appendChild(frontImage)
+    pokeFront.appendChild(frontLabel)
+    return pokeFront
+}
+
+function populateCardBack(pokemon){
+    let pokeBack = document.createElement('div')
+    pokeBack.className = 'card_face card_face--back'
+    let backLabel = document.createElement('p')
+    backLabel.textContent = `${pokemon.moves.length} moves`
+    pokeBack.appendChild(backLabel)
+    return pokeBack
+}
 function getImageFileName(pokemon){
     if (pokemon.id < 10) {
         return `00${pokemon.id}`
-    } else if (pokemon.id > 9 && pokemon.id < 100){
+    } else if (pokemon.id > 9 && pokemon.id < 100) {
         return `0${pokemon.id}`
     }
 }
